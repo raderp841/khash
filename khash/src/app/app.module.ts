faf5f04bb9362a97fcb48cd28e7a9e6aa1028765
peter rader <raderp841@gmail.com>
2020-03-11 03:07:00 -0400

    template work

--- khash/src/app/app.module.ts
+++ khash/src/app/app.module.ts
@@ -7,20 +7,26 @@ import { LayoutComponent } from './components/layout/layout.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ContentComponent } from './components/content/content.component';
-
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faInstagram, faFacebook, faTwitter,faLinkedin} from '@fortawesome/free-brands-svg-icons';
 @NgModule({
-  declarations: [
-    AppComponent,
-    LayoutComponent,
-    HeaderComponent,
-    FooterComponent,
-    ContentComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LayoutComponent,
+        HeaderComponent,
+        FooterComponent,
+        ContentComponent,
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FontAwesomeModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+    constructor(private library: FaIconLibrary) {
+        library.addIcons(faTwitter, faInstagram, faFacebook, faLinkedin);
+    }
+}
--- khash/src/app/components/content/content.component.ts
+++ khash/src/app/components/content/content.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-content',
-  templateUrl: './content.component.html',
-  styleUrls: ['./content.component.css']
+    selector: 'app-content',
+    templateUrl: './content.component.html',
+    styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
+    onClickFacebook() {
+        window.open('https://www.facebook.com/Liberty-Home-Mortgage-810999295604797/');
+    }
+
+    onClickInstagram() {
+        window.open('https://www.instagram.com/khash_saghafi/');
+    }
+
+    onClickLinkedin() {
+        window.open('https://www.linkedin.com/company/liberty-home-mortgage-corporation/');
+    }
+
+    onClickTwitter() {
+        window.open('https://twitter.com/finance_wizard?lang=en');
+    }
 }
